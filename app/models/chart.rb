9e105d2b37185e19b5aecccae2e34b3fdcede124
Marnen Laibow-Koser <marnen@marnen.org>
2020-04-03 16:05:55 -0400

    Create a Chart model to generate image-charts URLs. [#4]

---
+++ app/models/chart.rb
@@ -0,0 +1,15 @@
+class Chart
+  def initialize(values)
+    @values = values
+  end
+
+  def url
+    URI('https://image-charts.com/chart?').tap do |url|
+      params = {
+        cht: :lc, # line chart
+        chd: "a:#{@values.join ','}"
+      }
+      url.query = Faraday::Utils.build_query params
+    end
+  end
+end
---
+++ spec/models/chart_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Chart, type: :model do
+  describe 'constructor' do
+    it 'takes an array of values' do
+      expect(described_class.new []).to be_a_kind_of described_class
+    end
+  end
+
+  describe '#url' do
+    let(:values) { Array.new(rand 2..10) { rand 100 } }
+    let(:params) { Faraday::Utils.parse_query subject.query }
+
+    subject { described_class.new(values).url }
+
+    it 'points to the image-charts API endpoint' do
+      expect(subject.to_s).to start_with 'https://image-charts.com/chart?'
+    end
+
+    it 'builds a line chart' do
+      expect(params['cht']).to be == 'lc'
+    end
+
+    it 'puts the values into the data array' do
+      expect(params['chd']).to be == "a:#{values.join ','}"
+    end
+  end
+end
