05e5663ff3ed766c3cb6258381ae9e2f63865284
Marnen Laibow-Koser <marnen@marnen.org>
2020-04-07 15:15:07 -0400

    Remove code for generating image chart. [#5]

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -31,37 +31,6 @@ class Chart
     end
   end
 
-  def url
-    chart_type = CHART_TYPES[:line]
-    data = values
-    size = '800x600'
-    axes = 'x,y'
-    axis_labels = [start_date, end_date].map {|date| date.to_s :short }
-    axis_styles = "0,#{COLORS[:text]}|1,#{COLORS[:text]}"
-    position = LEGEND_POSITIONS[:top]
-    legend_color = COLORS[:text]
-    line_thickness = 3
-    background = "bg,s,#{COLORS[:background]}"
-
-    URI('https://image-charts.com/chart?').tap do |url|
-      params = {
-        cht: chart_type,
-        chd: "a:#{data.join ','}",
-        chs: size,
-        chxt: axes,
-        chxl: "0:#{axis_labels.map {|label| label.prepend '|' }.join }",
-        chxs: axis_styles,
-        chdl: @legend,
-        chdlp: position,
-        chdls: legend_color,
-        chco: COLORS[:series],
-        chls: line_thickness,
-        chf: background
-      }
-      url.query = URI.encode_www_form params
-    end
-  end
-
   private
 
   def dates
--- spec/models/chart_spec.rb
+++ spec/models/chart_spec.rb
@@ -106,57 +106,5 @@ RSpec.describe Chart, type: :model do
         end
       end
     end
-
-    describe '#url' do
-      let(:params) { Hash[URI.decode_www_form URI(subject).query] }
-
-      subject { chart.url }
-
-      it 'points to the image-charts API endpoint' do
-        expect(subject.to_s).to start_with 'https://image-charts.com/chart?'
-      end
-
-      it 'builds a line chart' do
-        expect(params['cht']).to be == 'lc'
-      end
-
-      it "sets the chart's size to 800x600" do
-        expect(params['chs']).to be == '800x600'
-      end
-
-      it 'puts the values into the data array, sorted by date' do
-        expect(params['chd']).to be == "a:#{sorted_pairs.map(&:last).join ','}"
-      end
-
-      it 'makes the x and y axes visible' do
-        expect(params['chxt']).to be == 'x,y'
-      end
-
-      it 'labels the x axis with the dates of the first and last pairs' do
-        start_date = chart.pairs.first.first
-        end_date = chart.pairs.last.first
-        expect(params['chxl']).to be == "0:|#{start_date.to_s :short}|#{end_date.to_s :short}"
-      end
-
-      it 'labels the data series with the legend string at top center, in the text color given in COLORS' do
-        expect(params).to include 'chdl' => legend, 'chdlp' => 't', 'chdls' => COLORS[:text]
-      end
-
-      it 'sets the data series color to the value in COLORS' do
-        expect(params['chco']).to be == COLORS[:series]
-      end
-
-      it 'sets the line thickness to 3' do
-        expect(params['chls'].to_i).to be == 3
-      end
-
-      it 'sets the background fill to the value in COLORS' do
-        expect(params['chf']).to be == "bg,s,#{COLORS[:background]}"
-      end
-
-      it 'sets the axis labels to the text color in COLORS' do
-        expect(params['chxs']).to be == "0,#{COLORS[:text]}|1,#{COLORS[:text]}"
-      end
-    end
   end
 end
