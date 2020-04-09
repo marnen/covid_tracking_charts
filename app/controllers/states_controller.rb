7cc66586ebff5e860768134a7ab72d42140537f4
Marnen Laibow-Koser <marnen@marnen.org>
2020-04-09 01:37:57 -0400

    Extract method. [#37]

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,19 +1,14 @@
 class StatesController < ApplicationController
   def show
-    params[:states] = params[:states].strip.downcase.split(%r{\W+})
-    state_abbrs = params[:states]
-    sorted_abbrs = state_abbrs.sort
-    unless sorted_abbrs == state_abbrs
-      redirect_to action: :show, states: sorted_abbrs.join(','), status: :moved_permanently and return
-    end
+    normalize_states
 
-    @states = State.find state_abbrs
+    @states = State.find params[:states]
     @date = Date.current
     date_range = (@date - 29.days)..@date
 
     query = Query.new states: @states, date_range: date_range
     @raw_data = query.raw_data
-    chart_data = @raw_data.transform_values do |requests|
+    chart_data = @raw_data.transform_values do |requests| # TODO: this should probably move into Query
       requests.map {|(_, response)| [Date.parse(response['date'].to_s), response['positive']] }
     end
 
@@ -22,5 +17,16 @@ class StatesController < ApplicationController
 
   def choose
     redirect_to action: :show, states: Array(params[:states]).join(',').downcase
+  end
+
+  private
+
+  def normalize_states
+    params[:states] = params[:states].strip.downcase.split(%r{\W+})
+    state_abbrs = params[:states]
+    sorted_abbrs = state_abbrs.sort
+    unless sorted_abbrs == state_abbrs
+      redirect_to action: :show, states: sorted_abbrs.join(','), status: :moved_permanently and return
+    end
   end
 end
