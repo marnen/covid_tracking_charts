401caf02949740db5a72506c44a9418978ccf4f3
Marnen Laibow-Koser <marnen@marnen.org>
2020-04-10 17:23:09 -0400

    Set time zone to Eastern. [#38]

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,6 @@ module CovidTrackingCharts
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.time_zone = 'Pacific Time (US & Canada)'
+    config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
