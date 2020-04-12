4defecf4b71b526a847bfa730d8c0c217d2b029f
Marnen Laibow-Koser <marnen@marnen.org>
2020-04-12 19:21:35 -0400

    Extract methods. [#47]

--- app/cells/state_list_cell.rb
+++ app/cells/state_list_cell.rb
@@ -7,5 +7,16 @@ class StateListCell < Cell::ViewModel
 
   private
 
-  attr_reader :states
+  def state_names_and_abbrs
+    @states.map.with_index(1) {|state, index| name_and_abbr state, index }
+  end
+
+  def name_and_abbr(state, index)
+    h(_'%{span}%{state_name}%{_span} (%{state_abbr})') % {
+      span: "<span class='state-#{index}'>".html_safe,
+      _span: '</span>'.html_safe,
+      state_name: _(state.name),
+      state_abbr: _(state.abbr)
+    }
+  end
 end
